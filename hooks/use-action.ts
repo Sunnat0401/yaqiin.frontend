dd4d89ec28a283135c274888306b5e4f20485f34
Sunnat0401 <sunnnatdasturchi@gmail.com>
2025-08-06 15:46:49 +0500

    New repo

--- hooks/use-action.ts
+++ hooks/use-action.ts
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import { toast } from './use-toast'
+import { useState } from "react";
+import { toast } from "./use-toast";
 
 const useAction = () => {
-	const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
-	function onError(message: string) {
-		setIsLoading(false)
-		toast({ description: message, variant: 'destructive' })
-	}
+  function onError(message: string) {
+    setIsLoading(false);
+    toast({ description: message, variant: "destructive" });
+  }
 
-	return { isLoading, setIsLoading, onError }
-}
+  return { isLoading, setIsLoading, onError };
+};
 
-export default useAction
+export default useAction;
