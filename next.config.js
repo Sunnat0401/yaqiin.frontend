68ec0c38c1481d1c52200c8effa9a6542cdfc510
Sunnat0401 <sunnnatdasturchi@gmail.com>
2025-08-06 15:57:55 +0500

    deploy

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const nextConfig = {
 	images: {
 		remotePatterns: [{ protocol: 'https', hostname: 'utfs.io', pathname: '**' }],
 	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
 }
 
 export default nextConfig
